836ec5bd3a17b6a394c86b0315be76a138bd7b39
SblTeam <venkatesh@tulassi.com>
2024-07-17 14:16:30 +0530

    Revert "Price master"

--- app/Models/oc_pricemaster.php
+++ app/Models/oc_pricemaster.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class oc_pricemaster extends Model
 {
     use HasFactory;
-
-    // Allow mass assignment for these fields
     protected $fillable = [
-      'id',
-        'date',
+        'fromdate',
+        'todate',
+        'customer',
+        'catgroup',
         'cat',
         'code',
         'desc',
@@ -22,13 +22,7 @@ class oc_pricemaster extends Model
         'updated',
         'client',
         'importkey',
-        'updated_at',
-        'created_at',
-        'incr',
-        'pm'
+
+
     ];
-
-    // Optionally, if you want to disable automatic timestamps
-    public $timestamps = true; // Set this to false if you want to manage timestamps manually
 }
-
